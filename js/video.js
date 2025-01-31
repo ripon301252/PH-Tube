5efc161e888428d8b4ce21410a1f0538f19350cb
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2025-01-31 21:33:23 +0600

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,3 +1,12 @@
+function getTimeString(time){
+    const day = parseInt (time/ 86400) // 1day = 86400 Second 
+    const hour = parseInt (time/3600 );    // 1hour = 3600 Second
+    let second = time % 3600;
+    const minuite =parseInt (second / 60);
+    second = second % 60;
+    return`${day} day ${hour} hour ${minuite} minuite ${second} second ago`
+}
+
 // 1st step 1, fetch & load data
 const loadCatgories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -10,17 +19,17 @@ const loadCatgories = () => {
 // 1st step 2, display data
 const displayCategories = (categories) => {
 // console.log(categories)
-    // display data 2nd step catch the DOM
+    // display data 1st time, catch the DOM
     const categoryContainer = document.getElementById('categories')
 
+    // display data 2nd time, creat a button by forEach Loop
     categories.forEach(item => {
     // console.log(item);
-        // display data 1st step, creat a button
         const button = document.createElement('button');
-        button.classList= 'btn ';
+        button.classList= 'btn';
         button.innerText = item.category
 
-        // display data 3rd step, Append (show)
+        // display data 3rd time, Append (show)
         categoryContainer.append(button);
     });
 }
@@ -40,40 +49,52 @@ const loadVideos = () => {
         .catch(err => console.log(err))
 }
 
-// 1st step 2, display data
+// 2nd step 2, display data
 const displayVideos = (videos) =>{
 // console.log(videos);
+    // display data 1st time, catch the DOM
+    const videoContainer = document.getElementById('videos');
 
-    // display data 2nd step catch the DOM
-    const videoContainer = document.getElementById('videos')
-
+    // display data 2nd time, creat a video cart by forEach Loop
     videos.forEach(video =>{
-    console.log(video);
-        // display data 1st step, creat a video cart
+    // console.log(video);
         const card = document.createElement('div');
         card.classList= 'card'
         card.innerHTML = `
-            <figure class='w-[280px] h-[200px]'>
-                <img class='rounded-xl w-ful h-full object-cover' src=${video.thumbnail} alt="Shoes" />
+            <figure class='h-[200px] relative'>
+                <img 
+                src=${video.thumbnail} 
+                class='rounded-xl w-ful h-full object-cover'  
+                alt="Shoes" />
+                ${
+                    video.others.posted_date?.length === 0 
+                    ? "" 
+                    : `<span class='absolute right-3 bottom-2 text-white bg-black p-1 rounded text-xs'>${ getTimeString(video.others.posted_date)}</span>`
+                }
             </figure>
+
             <div class="flex items-center px-0 py-3 gap-3 mb-10">
                 <div>
                     <img class='rounded-full w-12 h-12 object-cover' src=${video.authors[0].profile_picture} alt="Shoes" />
                 </div>
                 <div>
                     <h2> ${video.title}</h2>
+
                     <div class='flex items-center gap-3'>
                         <h2 class='font-bold'> ${video.authors[0].profile_name}</h2>
-                        <img class='rounded-full w-7  object-cover' src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png' alt="Shoes" />
+                        
+                        ${video.authors[0].verified === true 
+                        ?`<img class='rounded-full w-7  object-cover' src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png'/>` 
+                        : ""}
                     </div>
-                    
                 </div>
             </div>
-           
         `
-        // display data 3rd step, Append (show)
+
+        // display data 3rd time, Append (show)
         videoContainer.append(card);
     })
 }
 
 loadVideos()
+
