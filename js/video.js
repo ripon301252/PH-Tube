8cbdc806d5a7684f5cae9d8ebc85a062d68b4bd0
ripon301252 <93132951+ripon301252@users.noreply.github.com>
2025-02-03 15:30:42 +0600

    Update video.js

--- js/video.js
+++ js/video.js
@@ -17,7 +17,7 @@ const removeActiveClass = () =>{
 }
 
 
-// saperet video card by button click
+// saperet video by button click
 const loadCatgoryVideos = (id) => {
     // alert(id);
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
@@ -35,6 +35,33 @@ const loadCatgoryVideos = (id) => {
         })
         .catch(err => console.log(err))
 }
+
+// Load Details
+const loadDetails = async (videoId) =>{
+    // console.log(videoId);
+    const uri=`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    const res = await fetch(uri);
+    const data = await res. json();
+    displayDetails(data.video);
+}
+
+// display details
+const displayDetails = (video) => {
+    // console.log(video)
+    const detailContainer = document.getElementById('modal-content');
+    detailContainer.innerHTML = `
+        <img src=${video.thumbnail}/>
+        <p>${video.description}</p>
+    `;
+
+    // way-1
+        document.getElementById('showModalData').click();
+
+    // way-2
+        // document.getElementById('customModal').showModal();
+}
+
+//----------------------------------------------------------------------------
 
 // 1st step 1, fetch & load data
 const loadCatgories = () => {
@@ -130,29 +157,30 @@ const displayVideos = (videos) =>{
                     : `<span class='absolute right-3 bottom-2 text-white bg-black p-1 rounded text-xs'>${ getTimeString(video.others.posted_date)}</span>`
                 }
             </figure>
-
-            <div class="flex items-center px-0 py-3 gap-3 mb-10">
-                <div>
-                    <img class='rounded-full w-12 h-12 object-cover' src=${video.authors[0].profile_picture} alt="Shoes" />
-                </div>
-                <div>
-                    <h2> ${video.title}</h2>
-
-                    <div class='flex items-center gap-3'>
-                        <h2 class='font-bold'> ${video.authors[0].profile_name}</h2>
-                        
-                        ${video.authors[0].verified === true 
-                        ?`<img class='rounded-full w-7  object-cover' src='https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png'/>` 
-                        : ""}
-                    </div>
-                </div>
+            
+         <div class="px-0 py-2 flex gap-2">
+            <div>
+                <img class="w-10 h-10 rounded-full object-cover" src= ${video.authors[0].profile_picture} />
             </div>
-        `
+            <div>
+                <h2 class="font-bold">${video.title}</h2>
+                <div class="flex items-center gap-2">
+                    <p class="text-gray-400"> ${video.authors[0].profile_name}</p>
+                    ${video.authors[0].verified == true
+                    ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" /> `
+                    : ""}
+                    
+                </div>
+               
+                <p><button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-error mt-2">Details</button></p>
+            </div>
+        </div>
+        ` 
 
         // display data 3rd time, Append (show)
         videoContainer.append(card);           
-    })
-}
+    });
+};
 
 loadVideos();
 
